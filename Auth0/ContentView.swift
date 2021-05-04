4f5c11909a1c9a87e86e8873b439700d839d91fc
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-05-04 23:15:16 +0300

    Initial Commit

---
+++ Auth0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Auth0
+//
+//  Created by Necdet Alperen Özil on 4.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
